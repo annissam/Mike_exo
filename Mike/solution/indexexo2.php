12ffa8881d81480757ee4be2b6d4031870bd8db7
Etudiant <annissa.mekiri@lepoles.com>
2017-05-15 11:51:49 +0200

    first commit

---
+++ Mike/solution/indexexo2.php
@@ -0,0 +1,52 @@
+<?php
+	if(!empty($_POST)){ // le ! affiche le contraire
+		$mike = explode(" ",$_POST["Mike"]);
+
+		if(count($mike) < 3)
+			echo "Nombre de parametres pas assez important";
+		else if(count($mike) > 3)
+			echo "Nombre de paramètres trop important";
+		else if($mike[0] !="Mike")
+			echo"Premiere valeur incorrect";
+			else if( !is_int(intval($mike[1])) || ! is_int($mike[2])))
+			echo "Votre parametre n'ai pas correct";
+			else {
+				echo "Parametre 1: $mike[1]<br/>Parametre 2: $mike[2]";
+		}
+
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title> Piscine </title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<form method="POST">
+					<div class="form-group">
+						<label for="text">Entre votre valeur:</label>
+						<input type="text" class="form-control" id="Mike" name="Mike">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+			</div>
+		</div>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	</body>
+</html>
